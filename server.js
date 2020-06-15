364061759f6738b3cc0a10ff61f82342b2718b07
Tyler Bolty <tsbolty@gmail.com>
2020-06-15 16:24:24 -0600

    server running. working on 404 error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const logger = require("morgan");
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 require("dotenv").config();
 
 const app = express();
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-app.use(flash())
+// app.use(flash())
 app.use(express.static("public"));
 app.use(session({
     secret: "keyboard cat",
@@ -29,7 +29,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GoingsOn", { useNewUrlParser: true }, function(err) {
   if (err) throw err;
-  console.log(`mongoose connection successful`.yellow);
+  console.log(`mongoose connection successful`);
   
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
