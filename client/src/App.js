4ce45c1cb800793adbaed6482c69625bdedefeba
Tyler Bolty <tsbolty@gmail.com>
2020-06-18 18:16:50 -0600

    incorporated functioning user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 import ViewTest from './components/ViewTest'
 import LocationCard from './components/LocationCard'
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
+      <NavBar />
+      {/* <ViewTest /> */}
       <LocationCard />
     </div>
   );
