24d1cf56cc73ab9d4cffde54025779e8471cdaf9
Tyler Bolty <tsbolty@gmail.com>
2020-08-11 20:09:06 -0600

    cleaning up test page

--- client/src/components/ViewHappyHour.js
+++ client/src/components/ViewHappyHour.js
@@ -25,8 +25,11 @@ const ViewHappyHour = ()=>{
         {array.map(special=>(
             <>
                 <h3 className= "food-special-heading">{special.foodSpecial1Heading}</h3>
-                <p className= "food-special-description">{special.drinkSpecial1Description}</p>
-                <button onClick = {()=> handleDelete(special._id)}>Delete</button>
+                <p className= "food-special-description">{special.foodSpecial1Description}</p>
+                <h3 className= "drink-special-heading">{special.drinkSpecial1Heading}</h3>
+                <p className= "drink-special-description">{special.drinkSpecial1Description}</p>
+                <button className= "delete-food-special-button" onClick = {()=> handleDelete(special._id)}>Delete</button>
+                <br></br>
             </>
         ))}
         </>
