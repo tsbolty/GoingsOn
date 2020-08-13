3db163e1cfe81d169b94f7af9c39c022d60b44d6
Tyler Bolty <tsbolty@gmail.com>
2020-08-13 09:24:53 -0600

    working on grabbing state value

--- client/src/components/MondayFoodSpecials.js
+++ client/src/components/MondayFoodSpecials.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 
 const MondayFoodSpecials = ()=>{
-    const [array, setArray] = useState([])
+    const [array, setArray] = useState({value: ""})
     
     
     const gatherData = (e)=>{
         let nam = e.target.name
         let val = e.target.value
-        setArray({[nam]: val})
+        setArray({value: val})
     }
     
     const handleSubmit = ()=>{
-        console.log(array)
+        gatherData()
     }
 
     return(
         <form>
-            <input type= "text" name= "mondayFoodSpecialHeader" onChange= {()=> gatherData} placeholder= "Monday Food Special Title"></input>
-            {/* <input type= "text" name= "mondayFoodSpecialDescription" onChange= {()=> gatherData()} placeholder= "Monday Food Special Description"></input> */}
-            <button onClick= {()=> handleSubmit()}>Submit</button>
+            <label>
+                Monday:
+                <br />
+                <textarea type= "text" name= "mondayFoodSpecialHeader" onChange= {()=> gatherData} placeholder= "Monday Food Special Title"></textarea>
+                {/* <input type= "text" name= "mondayFoodSpecialDescription" onChange= {()=> gatherData()} placeholder= "Monday Food Special Description"></input> */}
+                <button onClick= {()=> handleSubmit()}>Submit</button>
+            </label>
         </form>
     )
 }
