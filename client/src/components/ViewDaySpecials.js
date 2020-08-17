4ad8ee240a94d942aa8a2b5ce871380835199a16
Tyler Bolty <tsbolty@gmail.com>
2020-08-16 20:34:00 -0600

    can gather daily specials

---
+++ client/src/components/ViewDaySpecials.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CreateHappyHour from './CreateHappyHour';
+
+const ViewDaySpecials = ({ day })=>{
+    const [daySpecialsData, setDaySpecialsData] = useState([])
+
+    const getDaySpecials = ()=>{
+        axios.get('/api/dailySpecials/get')
+        .then(res => setDaySpecialsData(res.data))
+    }
+
+    const logDaySpecials = ()=>{
+        console.log(daySpecialsData)
+    }
+
+    // useEffect(()=>{
+    //     getDaySpecials()
+    // }, [day])
+
+    const handleDelete = (id)=>{
+        axios.delete('/api/dailySpecials/delete/' + id)
+            .then(res=> getDaySpecials())
+    }
+
+    return(
+        <>
+        <div className= "card">
+            {daySpecialsData &&
+        daySpecialsData.map(special=>(
+            <>
+                <h3>{special.foodSpecialHeading}</h3>
+                <p>{special.foodSpecialDescription}</p>
+                <h3>{special.drinkSpecialHeading}</h3>
+                <p>{special.drinkSpecialDescription}</p>
+                <button className= "delete-food-special-button" onClick = {()=> handleDelete(special._id)}>Delete</button>
+                <br />
+            </>
+        ))}
+        <button onClick= {()=> getDaySpecials()}>Get</button>
+        <button onClick= {()=> logDaySpecials()}>Log</button>
+        </div>
+        </>
+    )
+}
+
+export default ViewDaySpecials;
