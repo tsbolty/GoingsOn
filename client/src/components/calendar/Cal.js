0b1d58b84b2fd5093e9c4356b9c8daca4757d9ad
Tyler Bolty <tsbolty@gmail.com>
2020-08-27 15:59:47 -0600

    stying calendar

--- client/src/components/calendar/Cal.js
+++ client/src/components/calendar/Cal.js
@@ -1,18 +1,40 @@
+// import React, { useState } from 'react';
+// import Calendar from 'react-calendar';
+
+// const Cal = () => {
+//   const [date, setDate] = useState({date: ""})
+
+//   const change = (date)=>{
+//     setDate({date: date})
+//   }
+
+//   return(
+//     <div>
+//       <Calendar onChange= {(value)=> setDate({date: value})} />
+//     </div>
+//   )
+// }
+
+// export default Cal;
+
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import Calendar from 'react-calendar'
 
-const Cal = () => {
-  const [date, setDate] = useState({date: ""})
+const Cal = ()=> {
+  const [value, setValue] = useState(new Date());
 
-  const change = (date)=>{
-    setDate({date: date})
+  function onChange(nextValue) {
+    setValue(nextValue);
   }
 
-  return(
-    <div>
-      <Calendar onChange= {(value)=> setDate({date: value})} />
-    </div>
-  )
+  return (
+    <Calendar
+      tileClassName= "calendar-button"
+      className= "calendar"
+      onChange={onChange}
+      value={value}
+    />
+  );
 }
 
 export default Cal;
