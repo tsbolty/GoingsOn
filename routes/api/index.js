02ef42ac0ca505ec715b7099249c026afb099b92
Tyler Bolty <tsbolty@gmail.com>
2020-12-03 19:21:16 -0700

    commented out happy hour routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,12 +3,12 @@ const dailySpecials = require('./DailySpecialsRoutes')
 const businessInfo = require('./BusinessInfoRoutes')
 const allBusinessInfo = require('./AllBusinessInfoRoutes')
 const weeklySpecials = require('./WeeklySpecialsRoutes')
-const happyhourRoutes = require('./HappyHourRoutes')
+// const happyhourRoutes = require('./HappyHourRoutes')
 
 router.use("/dailySpecials", dailySpecials)
 router.use("/businessInfo", businessInfo)
 router.use("/allBusinessInfo", allBusinessInfo)
 router.use("/weeklySpecials", weeklySpecials)
-router.use("/happyhour", happyhourRoutes)
+// router.use("/happyhour", happyhourRoutes)
 
 module.exports = router;
