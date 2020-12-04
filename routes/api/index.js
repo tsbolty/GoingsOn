8a5ad779f7c6ed6afd0da02c2e1253e2c9539637
Tyler Bolty <tsbolty@gmail.com>
2020-12-03 19:15:44 -0700

    moved happyhour roures and removed heroku postbuild

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
-const happyhourRoutes = require('./HappyHourRoutes')
 const dailySpecials = require('./DailySpecialsRoutes')
 const businessInfo = require('./BusinessInfoRoutes')
 const allBusinessInfo = require('./AllBusinessInfoRoutes')
 const weeklySpecials = require('./WeeklySpecialsRoutes')
+const happyhourRoutes = require('./HappyHourRoutes')
 
-router.use("/happyhour", happyhourRoutes)
 router.use("/dailySpecials", dailySpecials)
 router.use("/businessInfo", businessInfo)
 router.use("/allBusinessInfo", allBusinessInfo)
 router.use("/weeklySpecials", weeklySpecials)
+router.use("/happyhour", happyhourRoutes)
 
 module.exports = router;
