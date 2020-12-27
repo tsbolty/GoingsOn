e298286f92282a7b574f8d00cf6e861f1093653e
Tyler Bolty <tsbolty@gmail.com>
2020-12-26 17:18:41 -0700

    cleaned up create business info component

--- client/src/components/createProfile/CreateBusinessInfo.js
+++ client/src/components/createProfile/CreateBusinessInfo.js
@@ -7,37 +7,7 @@ import HeadlineCardContent from '../homePage/HeadlineCardContent';
 import axios from 'axios';
 
 const CreateBusinessInfo = ({ user, getInitialInfo, profileInfo, setProfileInfo }) => {
-  // const [profileInfo, setProfileInfo] = useState({
-  //   email: "",
-  //   businessName: "",
-  //   businessAddress: "",
-  //   businessType: "",
-  //   businessHeadline: "",
-  //   mapsLink: ""
-  // })
-
-  // const getInitialInfo = async ()=>{
-  //   let info;
-  //   try{
-  //     info = await axios.get(`/api/allBusinessInfo/get/email/${user.email}`)
-  //     setProfileInfo({
-  //       ...profileInfo,
-  //       email: info.data[0].email,
-  //       businessName: info.data[0].businessName,
-  //       businessAddress: info.data[0].businessAddress,
-  //       businessType: info.data[0].businessType,
-  //       businessHeadline: info.data[0].businessHeadline,
-  //       mapsLink: info.data[0].mapsLink
-  //     })
-  //   } catch(err){
-  //     info = {}
-  //     console.log("There's probably no information in the database yet")
-  //   }
-  // }
-
-  // useEffect(()=>{
-  //   getInitialInfo()
-  // }, [])
+ 
 
   const postAllBusinessInfo = () => {
     const mapAddress = profileInfo.businessAddress.replace(/\s+/g, "").toLowerCase()
