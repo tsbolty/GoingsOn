4856c10467dc347764d6fabd4a52e56ecffab9aa
Tyler Bolty <tsbolty@gmail.com>
2021-03-08 20:09:26 -0700

    cleaned up headline card

--- client/src/components/homePage/HeadlineCard.js
+++ client/src/components/homePage/HeadlineCard.js
@@ -1,107 +1,146 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Filter from '../filter/Filter';
-import HeadlineCardContent from './HeadlineCardContent';
-
+import React, { useState, useEffect } from "react";
+import Filter from "../filter/Filter";
+import HeadlineCardContent from "./HeadlineCardContent";
+// import { slideInLeft } from "react-animations";
 // THIS COMPONENT WILL RENDER ALL BUSINESS INFO. FILTEREDINFO WILL UPDATE WITH CORRECT FILTERED RESULTS BY NAME, BUT NEED A WAY TO MAKE THIS COMPONENT RERENDER WHEN FILTERED INFO CHANGES.
 
 const HeadlineCard = () => {
-  const [businessInfo, setBusinessInfo] = useState([])
-  const [filteredInfo, setFilteredInfo] = useState()
-  const [filterType, setFilterType] = useState("")
+	const [businessInfo, setBusinessInfo] = useState([]);
+	const [filteredInfo, setFilteredInfo] = useState();
+	const [filterType, setFilterType] = useState("");
 
-  useEffect(() => {
-    fetch('/api/allBusinessInfo/get')
-      .then(data => data.json())
-      .then(res => setBusinessInfo(res))
-  }, [])
+	useEffect(() => {
+		fetch("/api/allBusinessInfo/get")
+			.then((data) => data.json())
+			.then((res) => setBusinessInfo(res))
+			.catch((err) => console.log("fuck"));
+	}, []);
 
-  const handleFilterTypeClick = (e)=>{
-    setFilterType(e.target.name)
-  }
+	const handleFilterTypeClick = (e) => {
+		setFilterType(e.target.name);
+	};
 
-  const filterBusinessByName = (name)=>{
-    if(!name){
-      setFilteredInfo()
-    } else {
-      const filteredBusinesses = businessInfo.filter(business =>{
-        return business.businessName.toLowerCase().includes(name.toLowerCase())
-      })
-      setFilteredInfo(filteredBusinesses)
-    }
-  }
+	const filterBusinessByName = (name) => {
+		if (!name) {
+			setFilteredInfo();
+		} else {
+			const filteredBusinesses = businessInfo.filter((business) => {
+				return business.businessName.toLowerCase().includes(name.toLowerCase());
+			});
+			setFilteredInfo(filteredBusinesses);
+		}
+	};
 
-  const filterBusinessType = (type) =>{
-    if(type !== ""){
-    const filteredBusinesses = businessInfo.filter(business =>{
-      return business.businessType === type
-    })
-    setFilteredInfo(filteredBusinesses)
-    } else {
-      setFilteredInfo()
-    }
-  }
+	const filterBusinessType = (type) => {
+		if (type !== "") {
+			const filteredBusinesses = businessInfo.filter((business) => {
+				return business.businessType === type;
+			});
+			setFilteredInfo(filteredBusinesses);
+		} else {
+			setFilteredInfo();
+		}
+	};
 
-  const filterByKeyword = (keyword)=>{
-    if(keyword !== ""){
-      const filteredBusinesses = businessInfo.filter(business =>{
-        // return Object.values(business.weeklySpecials[0]).includes(keyword.toLowerCase())
-        const mondaySpecials = Object.values(business.weeklySpecials[0].monday)
-        const tuesdaySpecials = Object.values(business.weeklySpecials[0].tuesday)
-        const wednesdaySpecials = Object.values(business.weeklySpecials[0].wednesday)
-        const thursdaySpecials = Object.values(business.weeklySpecials[0].thursday)
-        const fridaySpecials = Object.values(business.weeklySpecials[0].friday)
-        const saturdaySpecials = Object.values(business.weeklySpecials[0].saturday)
-        const sundaySpecials = Object.values(business.weeklySpecials[0].sunday)
-        return (mondaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || tuesdaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || wednesdaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || thursdaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || fridaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || saturdaySpecials.toString().toLowerCase().includes(keyword.toLowerCase())
-        || sundaySpecials.toString().toLowerCase().includes(keyword.toLowerCase()))
-        // return business.weeklySpecials[0].join(" ").toLowerCase().includes(keyword.toLowerCase())
-      })
-      setFilteredInfo(filteredBusinesses)
-    }
-    else{
-      setFilteredInfo()
-    }
-  }
+	const filterByKeyword = (keyword) => {
+		if (keyword !== "") {
+			const filteredBusinesses = businessInfo.filter((business) => {
+				// return Object.values(business.weeklySpecials[0]).includes(keyword.toLowerCase())
+				const mondaySpecials = Object.values(business.weeklySpecials[0].monday);
+				const tuesdaySpecials = Object.values(
+					business.weeklySpecials[0].tuesday
+				);
+				const wednesdaySpecials = Object.values(
+					business.weeklySpecials[0].wednesday
+				);
+				const thursdaySpecials = Object.values(
+					business.weeklySpecials[0].thursday
+				);
+				const fridaySpecials = Object.values(business.weeklySpecials[0].friday);
+				const saturdaySpecials = Object.values(
+					business.weeklySpecials[0].saturday
+				);
+				const sundaySpecials = Object.values(business.weeklySpecials[0].sunday);
+				return (
+					mondaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					tuesdaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					wednesdaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					thursdaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					fridaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					saturdaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase()) ||
+					sundaySpecials
+						.toString()
+						.toLowerCase()
+						.includes(keyword.toLowerCase())
+				);
+				// return business.weeklySpecials[0].join(" ").toLowerCase().includes(keyword.toLowerCase())
+			});
+			setFilteredInfo(filteredBusinesses);
+		} else {
+			setFilteredInfo();
+		}
+	};
 
-  return (
-      <>
-        <Filter filterType= {filterType} handleFilterTypeClick= {handleFilterTypeClick} filterBusinessType= {filterBusinessType} filterBusinessByName= {filterBusinessByName} filterByKeyword= {filterByKeyword}/> 
-        <br/>
-        <br/>
-        {businessInfo && filteredInfo ? filteredInfo.map((item) => (
-          <HeadlineCardContent 
-          businessName= {item.businessName} 
-          id= {item._id} 
-          businessType= {item.businessType} 
-          businessAddress= {item.businessAddress}
-          daySpecials= {item.daySpecials} 
-          businessHeadline= {item.businessHeadline} 
-          weeklySpecials= {item.weeklySpecials} 
-          key= {item._id}
-          />
-        ))
-        :
-         businessInfo.map((item) => (
-          <HeadlineCardContent 
-          businessName= {item.businessName} 
-          id= {item._id} 
-          businessType= {item.businessType} 
-          businessAddress= {item.businessAddress} 
-          businessHeadline= {item.businessHeadline} 
-          daySpecials= {item.daySpecials} 
-          weeklySpecials= {item.weeklySpecials} 
-          key= {item._id}
-          />
-        ))
-      }
-      </>
-  )
-}
+	return (
+		<>
+			<Filter
+				filterType={filterType}
+				handleFilterTypeClick={handleFilterTypeClick}
+				filterBusinessType={filterBusinessType}
+				filterBusinessByName={filterBusinessByName}
+				filterByKeyword={filterByKeyword}
+			/>
+			<br />
+			<br />
+			{businessInfo && filteredInfo
+				? filteredInfo.map((item) => (
+						<div className='animate__lightSpeedInLeft'>
+							<HeadlineCardContent
+								businessName={item.businessName}
+								id={item._id}
+								businessType={item.businessType}
+								businessAddress={item.businessAddress}
+								daySpecials={item.daySpecials}
+								businessHeadline={item.businessHeadline}
+								weeklySpecials={item.weeklySpecials}
+								key={item._id}
+							/>
+						</div>
+				  ))
+				: businessInfo.map((item) => (
+						<div className='animate__lightSpeedInLeft'>
+							<HeadlineCardContent
+								businessName={item.businessName}
+								id={item._id}
+								businessType={item.businessType}
+								businessAddress={item.businessAddress}
+								businessHeadline={item.businessHeadline}
+								daySpecials={item.daySpecials}
+								weeklySpecials={item.weeklySpecials}
+								key={item._id}
+							/>
+						</div>
+				  ))}
+		</>
+	);
+};
 
 export default HeadlineCard;
--- client/src/components/homePage/HeadlineCardContent.js
+++ client/src/components/homePage/HeadlineCardContent.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
-const HeadlineCardContent = ({businessName, _id, businessType, businessAddress, businessHeadline,  daySpecials, weeklySpecials})=>{
-  let {id} = useParams();
-
-  return(
-    <div className="card headline-card" key= {_id}>
-      <h4>{`${businessName} (${businessType})`}</h4>
-      <h5>{businessHeadline}</h5>
-      <p>{businessAddress}</p>
-      {businessAddress &&
-      <a href= {`https://www.google.com/maps/search/?api=1&query=${businessAddress.replace(/ /gi, "+").toLowerCase()}`} target= "_blank">Google Maps</a>
-      }
-      <Link to= {{
-        pathname: `/infopage/${_id}`,
-        state: {businessInfo: {
-          businessName, _id, businessType, businessAddress, daySpecials, weeklySpecials
-        }}
-      }}
-      id= {id}>See All the Deals</Link>
-    </div>
-  )
-}
+const HeadlineCardContent = ({
+	businessName,
+	id,
+	businessType,
+	businessAddress,
+	businessHeadline,
+	daySpecials,
+	weeklySpecials
+}) => {
+	return (
+		<div className='card headline-card' key={id}>
+			<h4>{`${businessName} (${businessType})`}</h4>
+			<h5>{businessHeadline}</h5>
+			<p>{businessAddress}</p>
+			{businessAddress && (
+				<a
+					href={`https://www.google.com/maps/search/?api=1&query=${businessAddress
+						.replace(/ /gi, "+")
+						.toLowerCase()}`}
+					target='_blank'>
+					Google Maps
+				</a>
+			)}
+			<Link
+				to={{
+					pathname: `/infopage/${id}`,
+					state: {
+						businessInfo: {
+							businessName,
+							id,
+							businessType,
+							businessAddress,
+							daySpecials,
+							weeklySpecials
+						}
+					}
+				}}
+				id={id}>
+				See All the Deals
+			</Link>
+		</div>
+	);
+};
 
 export default HeadlineCardContent;
